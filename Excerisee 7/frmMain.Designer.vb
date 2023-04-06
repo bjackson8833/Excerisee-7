3e5e5bc7b81ed20dfb5544463d9b0e5040fa43c9
bjackson8833 <bjackson8833@W10-FML1NL3>
2023-04-06 10:03:04 -0500

    Help

--- Excerisee 7/frmMain.Designer.vb
+++ Excerisee 7/frmMain.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.groupBox1 = New System.Windows.Forms.GroupBox()
         Me.radioButton4 = New System.Windows.Forms.RadioButton()
@@ -33,14 +33,13 @@ Partial Class Form1
         Me.radNone = New System.Windows.Forms.RadioButton()
         Me.grpbSales = New System.Windows.Forms.GroupBox()
         Me.lblSub = New System.Windows.Forms.Label()
-        Me.label5 = New System.Windows.Forms.Label()
-        Me.label4 = New System.Windows.Forms.Label()
+        Me.lblSales = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
         Me.lblTotalD = New System.Windows.Forms.Label()
         Me.lblSaleD = New System.Windows.Forms.Label()
         Me.lblSubD = New System.Windows.Forms.Label()
         Me.btnCalc = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.groupBox1.SuspendLayout()
         Me.grbCofee.SuspendLayout()
         Me.grpbSales.SuspendLayout()
@@ -52,9 +51,11 @@ Partial Class Form1
         Me.groupBox1.Controls.Add(Me.radChocolate)
         Me.groupBox1.Controls.Add(Me.radSugar)
         Me.groupBox1.Controls.Add(Me.radGlazed)
-        Me.groupBox1.Location = New System.Drawing.Point(12, 26)
+        Me.groupBox1.Location = New System.Drawing.Point(9, 21)
+        Me.groupBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.groupBox1.Name = "groupBox1"
-        Me.groupBox1.Size = New System.Drawing.Size(254, 250)
+        Me.groupBox1.Padding = New System.Windows.Forms.Padding(2)
+        Me.groupBox1.Size = New System.Drawing.Size(190, 203)
         Me.groupBox1.TabIndex = 1
         Me.groupBox1.TabStop = False
         Me.groupBox1.Text = "Doughnut Choices"
@@ -62,9 +63,10 @@ Partial Class Form1
         'radioButton4
         '
         Me.radioButton4.AutoSize = True
-        Me.radioButton4.Location = New System.Drawing.Point(23, 203)
+        Me.radioButton4.Location = New System.Drawing.Point(17, 165)
+        Me.radioButton4.Margin = New System.Windows.Forms.Padding(2)
         Me.radioButton4.Name = "radioButton4"
-        Me.radioButton4.Size = New System.Drawing.Size(103, 20)
+        Me.radioButton4.Size = New System.Drawing.Size(85, 17)
         Me.radioButton4.TabIndex = 3
         Me.radioButton4.TabStop = True
         Me.radioButton4.Text = "&Filled ($1.50)"
@@ -73,9 +75,10 @@ Partial Class Form1
         'radChocolate
         '
         Me.radChocolate.AutoSize = True
-        Me.radChocolate.Location = New System.Drawing.Point(23, 150)
+        Me.radChocolate.Location = New System.Drawing.Point(17, 122)
+        Me.radChocolate.Margin = New System.Windows.Forms.Padding(2)
         Me.radChocolate.Name = "radChocolate"
-        Me.radChocolate.Size = New System.Drawing.Size(131, 20)
+        Me.radChocolate.Size = New System.Drawing.Size(109, 17)
         Me.radChocolate.TabIndex = 2
         Me.radChocolate.TabStop = True
         Me.radChocolate.Text = "C&hocolate ($1.25)"
@@ -84,9 +87,10 @@ Partial Class Form1
         'radSugar
         '
         Me.radSugar.AutoSize = True
-        Me.radSugar.Location = New System.Drawing.Point(23, 100)
+        Me.radSugar.Location = New System.Drawing.Point(17, 81)
+        Me.radSugar.Margin = New System.Windows.Forms.Padding(2)
         Me.radSugar.Name = "radSugar"
-        Me.radSugar.Size = New System.Drawing.Size(106, 20)
+        Me.radSugar.Size = New System.Drawing.Size(89, 17)
         Me.radSugar.TabIndex = 1
         Me.radSugar.TabStop = True
         Me.radSugar.Text = "&Sugar ($1.05)"
@@ -95,9 +99,10 @@ Partial Class Form1
         'radGlazed
         '
         Me.radGlazed.AutoSize = True
-        Me.radGlazed.Location = New System.Drawing.Point(23, 50)
+        Me.radGlazed.Location = New System.Drawing.Point(17, 41)
+        Me.radGlazed.Margin = New System.Windows.Forms.Padding(2)
         Me.radGlazed.Name = "radGlazed"
-        Me.radGlazed.Size = New System.Drawing.Size(113, 20)
+        Me.radGlazed.Size = New System.Drawing.Size(94, 17)
         Me.radGlazed.TabIndex = 0
         Me.radGlazed.TabStop = True
         Me.radGlazed.Text = "&Glazed ($1.05)"
@@ -108,9 +113,11 @@ Partial Class Form1
         Me.grbCofee.Controls.Add(Me.radCap)
         Me.grbCofee.Controls.Add(Me.radRegular)
         Me.grbCofee.Controls.Add(Me.radNone)
-        Me.grbCofee.Location = New System.Drawing.Point(9, 282)
+        Me.grbCofee.Location = New System.Drawing.Point(7, 229)
+        Me.grbCofee.Margin = New System.Windows.Forms.Padding(2)
         Me.grbCofee.Name = "grbCofee"
-        Me.grbCofee.Size = New System.Drawing.Size(257, 167)
+        Me.grbCofee.Padding = New System.Windows.Forms.Padding(2)
+        Me.grbCofee.Size = New System.Drawing.Size(193, 136)
         Me.grbCofee.TabIndex = 2
         Me.grbCofee.TabStop = False
         Me.grbCofee.Text = "Coffee Choices"
@@ -118,9 +125,10 @@ Partial Class Form1
         'radCap
         '
         Me.radCap.AutoSize = True
-        Me.radCap.Location = New System.Drawing.Point(23, 129)
+        Me.radCap.Location = New System.Drawing.Point(17, 105)
+        Me.radCap.Margin = New System.Windows.Forms.Padding(2)
         Me.radCap.Name = "radCap"
-        Me.radCap.Size = New System.Drawing.Size(142, 20)
+        Me.radCap.Size = New System.Drawing.Size(118, 17)
         Me.radCap.TabIndex = 6
         Me.radCap.TabStop = True
         Me.radCap.Text = "&Cappuccino ($2.75)"
@@ -129,9 +137,10 @@ Partial Class Form1
         'radRegular
         '
         Me.radRegular.AutoSize = True
-        Me.radRegular.Location = New System.Drawing.Point(23, 86)
+        Me.radRegular.Location = New System.Drawing.Point(17, 70)
+        Me.radRegular.Margin = New System.Windows.Forms.Padding(2)
         Me.radRegular.Name = "radRegular"
-        Me.radRegular.Size = New System.Drawing.Size(118, 20)
+        Me.radRegular.Size = New System.Drawing.Size(98, 17)
         Me.radRegular.TabIndex = 5
         Me.radRegular.TabStop = True
         Me.radRegular.Text = "&Regular ($1.50)"
@@ -140,9 +149,10 @@ Partial Class Form1
         'radNone
         '
         Me.radNone.AutoSize = True
-        Me.radNone.Location = New System.Drawing.Point(23, 42)
+        Me.radNone.Location = New System.Drawing.Point(17, 34)
+        Me.radNone.Margin = New System.Windows.Forms.Padding(2)
         Me.radNone.Name = "radNone"
-        Me.radNone.Size = New System.Drawing.Size(61, 20)
+        Me.radNone.Size = New System.Drawing.Size(51, 17)
         Me.radNone.TabIndex = 4
         Me.radNone.TabStop = True
         Me.radNone.Text = "&None"
@@ -151,107 +161,108 @@ Partial Class Form1
         'grpbSales
         '
         Me.grpbSales.Controls.Add(Me.lblSub)
-        Me.grpbSales.Controls.Add(Me.label5)
-        Me.grpbSales.Controls.Add(Me.label4)
+        Me.grpbSales.Controls.Add(Me.lblSales)
+        Me.grpbSales.Controls.Add(Me.lblTotal)
         Me.grpbSales.Controls.Add(Me.lblTotalD)
         Me.grpbSales.Controls.Add(Me.lblSaleD)
         Me.grpbSales.Controls.Add(Me.lblSubD)
-        Me.grpbSales.Location = New System.Drawing.Point(283, 26)
+        Me.grpbSales.Location = New System.Drawing.Point(212, 21)
+        Me.grpbSales.Margin = New System.Windows.Forms.Padding(2)
         Me.grpbSales.Name = "grpbSales"
-        Me.grpbSales.Size = New System.Drawing.Size(177, 256)
+        Me.grpbSales.Padding = New System.Windows.Forms.Padding(2)
+        Me.grpbSales.Size = New System.Drawing.Size(133, 208)
         Me.grpbSales.TabIndex = 3
         Me.grpbSales.TabStop = False
         '
         'lblSub
         '
         Me.lblSub.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.lblSub.Location = New System.Drawing.Point(81, 30)
+        Me.lblSub.Location = New System.Drawing.Point(61, 24)
+        Me.lblSub.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblSub.Name = "lblSub"
-        Me.lblSub.Size = New System.Drawing.Size(78, 39)
+        Me.lblSub.Size = New System.Drawing.Size(58, 32)
         Me.lblSub.TabIndex = 5
         '
-        'label5
+        'lblSales
         '
-        Me.label5.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.label5.Location = New System.Drawing.Point(81, 111)
-        Me.label5.Name = "label5"
-        Me.label5.Size = New System.Drawing.Size(78, 39)
-        Me.label5.TabIndex = 4
+        Me.lblSales.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.lblSales.Location = New System.Drawing.Point(61, 90)
+        Me.lblSales.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblSales.Name = "lblSales"
+        Me.lblSales.Size = New System.Drawing.Size(58, 32)
+        Me.lblSales.TabIndex = 4
         '
-        'label4
+        'lblTotal
         '
-        Me.label4.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.label4.Location = New System.Drawing.Point(81, 195)
-        Me.label4.Name = "label4"
-        Me.label4.Size = New System.Drawing.Size(78, 39)
-        Me.label4.TabIndex = 3
+        Me.lblTotal.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.lblTotal.Location = New System.Drawing.Point(61, 158)
+        Me.lblTotal.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(58, 32)
+        Me.lblTotal.TabIndex = 3
         '
         'lblTotalD
         '
         Me.lblTotalD.AutoSize = True
-        Me.lblTotalD.Location = New System.Drawing.Point(4, 195)
+        Me.lblTotalD.Location = New System.Drawing.Point(3, 158)
+        Me.lblTotalD.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblTotalD.Name = "lblTotalD"
-        Me.lblTotalD.Size = New System.Drawing.Size(69, 16)
+        Me.lblTotalD.Size = New System.Drawing.Size(57, 13)
         Me.lblTotalD.TabIndex = 2
         Me.lblTotalD.Text = "Total Due:"
         '
         'lblSaleD
         '
         Me.lblSaleD.AutoSize = True
-        Me.lblSaleD.Location = New System.Drawing.Point(4, 115)
+        Me.lblSaleD.Location = New System.Drawing.Point(3, 93)
+        Me.lblSaleD.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblSaleD.Name = "lblSaleD"
-        Me.lblSaleD.Size = New System.Drawing.Size(71, 16)
+        Me.lblSaleD.Size = New System.Drawing.Size(57, 13)
         Me.lblSaleD.TabIndex = 1
         Me.lblSaleD.Text = "Sales Tax:"
         '
         'lblSubD
         '
         Me.lblSubD.AutoSize = True
-        Me.lblSubD.Location = New System.Drawing.Point(6, 30)
+        Me.lblSubD.Location = New System.Drawing.Point(4, 24)
+        Me.lblSubD.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblSubD.Name = "lblSubD"
-        Me.lblSubD.Size = New System.Drawing.Size(65, 16)
+        Me.lblSubD.Size = New System.Drawing.Size(53, 13)
         Me.lblSubD.TabIndex = 0
         Me.lblSubD.Text = "SubTotal:"
         '
         'btnCalc
         '
-        Me.btnCalc.Location = New System.Drawing.Point(292, 298)
+        Me.btnCalc.Location = New System.Drawing.Point(219, 242)
+        Me.btnCalc.Margin = New System.Windows.Forms.Padding(2)
         Me.btnCalc.Name = "btnCalc"
-        Me.btnCalc.Size = New System.Drawing.Size(168, 72)
+        Me.btnCalc.Size = New System.Drawing.Size(126, 58)
         Me.btnCalc.TabIndex = 4
         Me.btnCalc.Text = "&Calculate"
         Me.btnCalc.UseVisualStyleBackColor = True
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(290, 389)
+        Me.btnExit.Location = New System.Drawing.Point(218, 316)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(2)
         Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(172, 49)
+        Me.btnExit.Size = New System.Drawing.Size(129, 40)
         Me.btnExit.TabIndex = 5
         Me.btnExit.Text = "E&xit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
-        'Button1
+        'frmMain
         '
-        Me.Button1.Location = New System.Drawing.Point(153, 189)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(168, 72)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = "&Calculate"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(474, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(356, 366)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnCalc)
         Me.Controls.Add(Me.grpbSales)
         Me.Controls.Add(Me.grbCofee)
         Me.Controls.Add(Me.groupBox1)
-        Me.Name = "Form1"
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "frmMain"
         Me.Text = "Form1"
         Me.groupBox1.ResumeLayout(False)
         Me.groupBox1.PerformLayout()
@@ -274,12 +285,11 @@ Partial Class Form1
     Private WithEvents radNone As RadioButton
     Private WithEvents grpbSales As GroupBox
     Private WithEvents lblSub As Label
-    Private WithEvents label5 As Label
-    Private WithEvents label4 As Label
+    Private WithEvents lblSales As Label
+    Private WithEvents lblTotal As Label
     Private WithEvents lblTotalD As Label
     Private WithEvents lblSaleD As Label
     Private WithEvents lblSubD As Label
     Private WithEvents btnCalc As Button
     Private WithEvents btnExit As Button
-    Private WithEvents Button1 As Button
 End Class
---
+++ Excerisee 7/frmMain.vb
@@ -0,0 +1,81 @@
+﻿' Name: Excerise 7
+' Purpose: Displays doughnut and coffee cost.
+' Programmer: Bj Jackson on 4/5/23
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim dblGlazed As Double
+        Dim dblSugar As Double
+        Dim dblChocolate As Double
+        Dim dblFilled As Double
+        Dim dblNone As Double
+        Dim dblRegular As Double
+        Dim dblCappuccino As Double
+        Dim dblSales As Double
+
+
+
+
+        dblCappuccino = 2.75
+        dblGlazed = 1.05
+        dblSugar = 1.05
+        dblChocolate = 1.25
+        dblFilled = 1.5
+        dblNone = 0
+        dblRegular = 1.5
+        dblSales = 0.06D
+
+        If radGlazed.Checked And radNone.Checked = True Then
+            GetCostGN(dblGlazed, dblSales, dblNone)
+
+        ElseIf radGlazed.Checked And radRegular.Checked = True Then
+            GetCostGR(dblGlazed, dblSales, dblRegular)
+
+
+
+        End If
+
+
+
+
+
+    End Sub
+    Private Sub GetCostGN(ByVal dblGlazed As Double, ByVal dblNone As Double, ByVal dblSales As Double)
+        Dim dblTotal As Double
+        Dim dblSub As Double
+        Dim dblTax As Double
+
+        dblSub = dblGlazed + dblNone
+        dblTax = (dblGlazed + dblNone) * dblSales
+        dblTotal = dblSub + dblTax
+        lblSales.Text = dblTax.ToString("C2")
+        lblTotal.Text = dblTotal.ToString("C2")
+        lblSub.Text = dblSub.ToString("C2")
+    End Sub
+
+    Private Sub GetCostGR(ByVal dblGlazed As Double, ByVal dblRegular As Double, ByVal dblSales As Double)
+        Dim dblTotal As Double
+        Dim dblSub As Double
+        Dim dblTax As Double
+
+        dblSub = dblGlazed + dblRegular
+        lblSub.Text = dblSub.ToString("C2")
+        dblTax = (dblGlazed + dblRegular) * dblSales
+        lblSales.Text = dblTax.ToString("C2")
+        dblTotal = dblSub + dblTax
+        lblTotal.Text = dblTotal.ToString("C2")
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
